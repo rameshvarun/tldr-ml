4225d2c765529d21ba30cb14893fa66d47936e62
Varun Ramesh <varunramesh@live.com>
2017-05-29 17:38:05 -0700

    Search functionality.

--- build-terms.py
+++ build-terms.py
@@ -3,19 +3,13 @@ import glob
 import yaml
 import json
 
-index = {}
-index['terms'] = {}
-index['aliases'] = {}
+terms = []
 for term_file in glob.iglob("terms/**/*.yml", recursive=True):
     id = os.path.splitext(os.path.basename(term_file))[0]
     with open(term_file, 'r') as file:
         doc = yaml.load(file)
-    index['terms'][id] = doc
+    doc['id'] = id
+    terms.append(doc)
 
-    index['aliases'][doc['term'].lower()] = id
-    if 'aliases' in doc:
-        for alias in doc['aliases']:
-            index['aliases'][alias.lower()] = id
-
-with open("index.json", 'w') as out:
-    out.write(json.dumps(index))
+with open("terms.json", 'w') as out:
+    out.write(json.dumps(terms))
--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 $(function() {
   var template = $("#term-template");
-  $.getJSON("index.json", function(data) {
-    for (var term of Object.values(data.terms)) {
+  var search = $("#search");
+  $.getJSON("terms.json", function(terms) {
+    for (var term of terms) {
       var entry = template.clone();
+      term.entry = entry;
+
       entry.find("#term-name").html(term.term);
       entry.find("#term-definition").html(term.definition);
+      entry.addClass("term");
       entry.show();
       entry.insertAfter(template);
     }
 
-    var fuse = new Fuse(data.terms, {
-      i
+    var fuse = new Fuse(terms, {
+      keys: ['term']
+    });
+
+    search.on('input', function(value) {
+      var val = search.val();
+      if (val.trim() === "") {
+        $(".term").show();
+      } else {
+        $(".term").hide();
+        var results = fuse.search(val);
+        for (result of results) result.entry.show();
+      }
     })
   });
 });
