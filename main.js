6faebc5b44ae5c43f9a14dfe27558f7453f5cba6
Varun Ramesh <varunramesh@live.com>
2017-05-30 02:35:34 -0700

    Search is reflected in hash.

--- main.js
+++ main.js
@@ -3,18 +3,6 @@ $(function() {
   var noresults = $("#no-results");
   var termentries = $(".term-entry");
 
-  function navigate() {
-    var hash = window.location.hash;
-    if (hash.length > 1 && hash[0] == "#") {
-      var id = hash.substring(1);
-      termentries.hide();
-      $("[data-term-id='" + id + "']").show();
-    }
-  }
-  navigate();
-
-  $(window).on('hashchange',function(){ navigate(); });
-
   $.getJSON("terms.json", function(terms) {
     for (var term of terms) {
       term.entry = $("[data-term-id='" + term.id + "']");
@@ -26,18 +14,34 @@ $(function() {
       shouldSort: true
     });
 
+    function navigate() {
+      var hash = window.location.hash;
+      if (hash.length >= 3 && hash.substr(0, 3) == "#q=") {
+        var query = decodeURIComponent(window.location.hash.substr(3));
+        if (query.trim() === "") {
+          termentries.show();
+          noresults.hide();
+        } else {
+          termentries.hide();
+          var results = fuse.search(query);
+          for (result of results) result.entry.show();
+          if (results.length == 0) noresults.show();
+          else noresults.hide();
+        }
+      }
+      else if (hash.length >= 2 && hash[0] == "#") {
+        var id = hash.substring(1);
+        termentries.hide();
+        $("[data-term-id='" + id + "']").show();
+      }
+    }
+    navigate();
+
+    $(window).on('hashchange',function(){ navigate(); });
+
     search.on('input', function(value) {
       var val = search.val();
-      if (val.trim() === "") {
-        termentries.show();
-        noresults.hide();
-      } else {
-        termentries.hide();
-        var results = fuse.search(val);
-        for (result of results) result.entry.show();
-        if (results.length == 0) noresults.show();
-        else noresults.hide();
-      }
-    })
+      window.location.hash = "q=" + encodeURIComponent(val);
+    });
   });
 });
